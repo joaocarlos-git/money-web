de6b03f95ecfb84d222155b31015ac32da067de1
João Carlos Silva Ribeiro <jocasiribeiro@hotmail.com>
2024-05-10 16:19:15 -0300

    atualização index.js e api.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home.jsx";
 import CadDespesa from "./pages/cad-despesa/cad-despesa.jsx";
 import "./styles/global.css";
-//import React from 'react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.15.6:3001"
+    baseURL: "http://192.168.0.55:3001"
 });
 
 export default api;
